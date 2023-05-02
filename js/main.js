bdccadd2ef86e81eded20895489763bef440374b
davidetaribelli <davide.taribelli@gmail.com>
2023-05-02 16:00:32 +0200

    MILESTONE 2

---
+++ js/main.js
@@ -0,0 +1,61 @@
+//VARIABILI
+const rowEl = document.querySelector(".row");
+
+//  creo l'array di oggetti con i dati forniti
+const team = [
+    //primo membro del team
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        foto: "../img/wayne-barnett-founder.jpg"
+    },
+    //secondo membro del team
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        foto: "../img/angela-caroll-chief-editor.jpg"
+    },
+    //terzo membro del team
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        foto: "../img/walter-gordon-office-manager.jpg"
+    },
+    //quarto membro del team
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        foto: "../img/angela-lopez-social-media-manager.jpg"
+    },
+    //quinto membro del team
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        foto: "../img/scott-estrada-developer.jpg"
+    },
+    //sesto membro del team
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        foto: "../img/barbara-ramos-graphic-designer.jpg"
+    }
+]
+
+
+console.log(team);
+
+
+for (let i = 0; i < team.length; i++){
+    const members = team[i];
+    let cardEl = document.createElement("div")
+    cardEl.classList.add("card");
+
+    for(const key in members){
+        let member = document.createElement("div");
+        member.innerHTML = (members[key]);
+        cardEl.appendChild(member)
+    }
+    
+    rowEl.appendChild(cardEl);
+
+}
